2513f2ecf6fd9edd74cfb4436d073986d853bc81
zediwards <zediwards@protonmail.com>
2023-12-11 13:45:41 -0500

    initial commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,20 @@
+import { Inter, Lusitana, Lobster, Lora, Merriweather } from "next/font/google";
+
+export const inter = Inter({ subsets: ["latin"] });
+export const lusitana = Lusitana({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+});
+
+// export const lobster = Lobster({ subsets: ["latin"] });
+
+export const lora = Lora({
+  subsets: ["latin"],
+  variable: "--font-headings",
+}); //Headings
+
+export const merriweather = Merriweather({
+  weight: "400",
+  subsets: ["latin"],
+  variable: "--font-body",
+}); //Body
